69bbcb6b1fdf7a5bd404657fcf0d98c17d515d16
SabbirHossainEvan <evansabbir31@gmail.com>
2025-11-09 21:04:12 +0600

    service section

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import ServicesSection from "./components/ServicesSection";
 
 export default function Home() {
   return (
-    <h1>ami evan</h1>
+    <ServicesSection></ServicesSection>
   );
 }
